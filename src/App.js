e96ad3ce48a18ff18af2e006524df8af83c817bc
youmari <youmari300@gmail.com>
2022-01-04 11:22:10 +0100

    Style the calculator component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import React from 'react';
+import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Hello world</h1>
-    </div>
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
