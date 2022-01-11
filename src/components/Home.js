102e4eef8c96d6d9ecc838474feb201e2e8fdf89
youmari <youmari300@gmail.com>
2022-01-11 09:03:55 +0100

    Add home page component and style it

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
+const Home = () => (
+  <div>
+    <h2 className="home-title">Welcome to our page</h2>
+    <p className="lorem">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, sequi
+      dolorum reiciendis repudiandae, voluptatum molestias veritatis eveniet
+      excepturi iusto dolores eaque obcaecati ab tenetur reprehenderit nemo,
+      labore illum recusandae architecto?
+    </p>
+    <p className="lorem">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, sequi
+      dolorum reiciendis repudiandae, voluptatum molestias veritatis eveniet
+      excepturi iusto dolores eaque obcaecati ab tenetur reprehenderit nemo,
+      labore illum recusandae architecto?
+    </p>
+  </div>
+);
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
-
-export default Home
+export default Home;
