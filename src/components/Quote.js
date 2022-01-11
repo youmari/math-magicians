610f2cad88488ab3858aa38b4fa4572628912774
youmari <youmari300@gmail.com>
2022-01-11 09:31:46 +0100

    Add quote page and styles it

---
+++ src/components/Quote.js
@@ -0,0 +1,12 @@
+const Quote = () => (
+  <div className="quote">
+    <p>
+      Without mathematics, there’s nothing you can do. Everything around you is
+      mathematics. Everything around you is numbers.
+      {' '}
+      <strong>— Shakuntala Devi, Indian writer and mental calculator</strong>
+    </p>
+  </div>
+);
+
+export default Quote;
